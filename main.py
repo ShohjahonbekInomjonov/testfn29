1771de0bfc5edbd2310c5958bef7bf81568e60ed
Shohjahon <inomjonovshohjahon@gmail.com>
2025-07-08 09:42:23 +0500

    class person ni qo'shdik

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ print("Hello World!")
 
 for i in range(1, 11):
     print(i ** 2, end=", ")
+
+class Person:
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age
+
